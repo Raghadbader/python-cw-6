5b0febb20e3e3ee605d6cbf1442752a7277c5877
Raghadbader <142684190+Raghadbader@users.noreply.github.com>
2023-09-06 19:46:04 +0300

    raghad

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # write your code here
+person = { 
+    "name": "raghad" ,
+    "age" : 18 ,
+
+    "hobbies" : ["sleep","watching movies"]
+
+}       
+
+print(person["name"])
+print(person["age"])
+
+person["age"] = 19
+person["country"] = "kuwait"
+
+print(person)
+person.items()
+print(person.items())
+
+person["hobbies"].append("sport") 
+
+def check_hobbies(person) :
+    if len(person["hobbies"])>=3 :
+        print("wow you are amazing")
+    else :
+        False
+check_hobbies(person)
+
+
+
